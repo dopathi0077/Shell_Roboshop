3cf36f921a4fb49d1d02000f75052b2f20de241d
varundopathi <varun.reddy0077@gmail.com>
2025-05-29 22:43:43 +0530

    added new file

--- catalogue.sh
+++ catalogue.sh
@@ -43,7 +43,7 @@ dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Install Node Js"
 
 id roboshop
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then
    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
    VALIDATE $? "Creating roboshop system user"
