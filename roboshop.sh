38818bb32e41ac3cca570f73d985d89ef9a3ce77
varundopathi <varun.reddy0077@gmail.com>
2025-05-29 15:07:12 +0530

    added new file

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+AMI_ID="ami-09c813fb71547fc4f"
+SG_IS="sg-02f1ec8684548b321"
+INSTANCES=("msql" "redis" "mongdb" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z031105925YL64MCO7Z8N"
+DOMAIN_NAME="varundopathi.site"
+
+#for instance in ${INSTANCES[@]}
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+    echo "$instance IP address: $IP"
