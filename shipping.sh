cbec609b0e18d122a0ca4b9d294d7ef90580370d
varundopathi <varun.reddy0077@gmail.com>
2025-05-30 15:48:36 +0530

    added new file

--- shipping.sh
+++ shipping.sh
@@ -36,42 +36,41 @@ VALIDATE(){
 }
 
 dnf install maven -y &>>$LOG_FILE
-VALIDATE $? "Installing Maven"
+VALIDATE $? "Installing Maven and Java"
 
-id roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-   VALIDATE $? "Creating roboshop system user"
-else 
-   echo "Roboshop user already.So $Y Skipping $N"
-fi 
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+fi
 
-mkdir -p /app  
-VALIDATE $? "creating app directory"
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
 
-curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip  &>>$LOG_FILE
-VALIDATE $? "Dwonloading shipping"
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading shipping"
 
 rm -rf /app/*
 cd /app 
 unzip /tmp/shipping.zip &>>$LOG_FILE
-VALIDATE $? "unzip shipping"
+VALIDATE $? "unzipping shipping"
 
-mvn clean package &>>$LOG_FILE
-VALIDATE $? "packaging the shipping application" 
+mvn clean package  &>>$LOG_FILE
+VALIDATE $? "Packaging the shipping application"
 
-mv target/shipping-1.0.jar shipping.jar  
-VALIDATE $? "moving and renaming the shipping jar" 
+mv target/shipping-1.0.jar shipping.jar  &>>$LOG_FILE
+VALIDATE $? "Moving and renaming Jar file"
 
-cp $SCRIPT_DIR/shipping.sh /etc/systemd/system/shipping.service &>>$LOG_FILE
-VALIDATE $? "copying shipping service file"
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
 
 systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "daemon-reload of shipping "
+VALIDATE $? "Daemon Realod"
 
 systemctl enable shipping  &>>$LOG_FILE
-VALIDATE $? "Enabling  shipping "
+VALIDATE $? "Enabling Shipping"
 
 systemctl start shipping &>>$LOG_FILE
 VALIDATE $? "Starting Shipping"
@@ -79,12 +78,12 @@ VALIDATE $? "Starting Shipping"
 dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "Install MySQL"
 
-mysql -h mysql.daws84s.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+mysql -h mysql.VarunDopathi.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
-    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    mysql -h mysql.VarunDopathi.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.VarunDopathi.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h mysql.VarunDopathi.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
     VALIDATE $? "Loading data into MySQL"
 else
     echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
@@ -93,8 +92,7 @@ fi
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restart shipping"
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-SCRIPT_END_TIME=$(date +%s)
-TOTAL_TIME=$(( $SCRIPT_END_TIME-$SCRIPT_START_TIME ))
-
-echo -e "Script executed succesfully $Y Time Taken: $TOTAL_TIME $N " | tee -a  $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
