101a4f94394c682eefd4a40efcf79131f08b02ec
varundopathi <varun.reddy0077@gmail.com>
2025-05-31 19:07:26 +0530

    added new file

--- shipping.sh
+++ shipping.sh
@@ -83,9 +83,9 @@ VALIDATE $? "Install MySQL"
 # mysql -h mysql.varundopathi.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' 
 # if [ $? -ne 0 ]
 # then
-    mysql -h mysql.varundopathi.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h mysql.varundopathi.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
-    mysql -h mysql.varundopathi.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    mysql -h mysql.varundopathi.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql 
+    mysql -h mysql.varundopathi.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  
+    mysql -h mysql.varundopathi.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql
     VALIDATE $? "Loading data into MySQL"
 # else
 #     echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
