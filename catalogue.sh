442337057d2b97e1affa13ef2635a46b21a45342
varundopathi <varun.reddy0077@gmail.com>
2025-05-31 15:02:22 +0530

    added new file

--- catalogue.sh
+++ catalogue.sh
@@ -82,10 +82,10 @@ VALIDATE $? "Install mongodb client"
 mongosh --host mongodb.VarunDopathi.site </app/db/master-data.js  &>>$LOG_FILE
 VALIDATE $? "Loading data into mongodb"
 
-STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.VarunDopathi.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.VarunDopathi.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
